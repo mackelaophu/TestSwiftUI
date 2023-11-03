796d581f166ba91ba6242f4661b791bb0160310e
dangph3 <dangph3@vpbank.com.vn>
2023-11-03 08:45:00 +0700

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by PhamHai Dang on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
